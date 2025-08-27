41e3f96cbde59114d013ae1f66f2f774946f953f
liqnerd <polaklxx@gmail.com>
2025-08-27 14:56:48 +0200

    Fix CORS for new Vercel domain to enable registration

--- cms/config/middlewares.ts
+++ cms/config/middlewares.ts
@@ -5,7 +5,13 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['https://dojdu.vercel.app', 'http://localhost:3000'],
+      origin: [
+        'https://dojdu.vercel.app', 
+        'https://dojdu-an0fvd2e6-polaklxx-4230s-projects.vercel.app',
+        'http://localhost:3000',
+        'http://localhost:3001',
+        /https:\/\/dojdu.*\.vercel\.app$/
+      ],
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
